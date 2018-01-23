dd065d5773d1092eed15f356e7c91c7216e488d2
MrAli•Gorithm <alisher.coder@gmail.com>
2018-01-22 22:26:24 -0600

    zoomFunctionModified
    tripCoordinate check added to the if statement

--- ride-along-development/Extension/HomeVCExt/HomeVCLocationManagerExtension.swift
+++ ride-along-development/Extension/HomeVCExt/HomeVCLocationManagerExtension.swift
@@ -24,9 +24,24 @@ extension HomeVC: CLLocationManagerDelegate {
     }
     
     func centerMapOnUserLocation() {
-        
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(mapView.userLocation.coordinate, regionRadius * 2.0, regionRadius * 2.0)
-        mapView.setRegion(coordinateRegion, animated: true)
+        guard let currentUserID = Auth.auth().currentUser?.uid else {
+            return
+        }
+        DataService.instance.REF_USERS.observeSingleEvent(of: .value) { (snapshot) in
+            if let userSnapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for user in userSnapshot {
+                    if user.key == currentUserID {
+                        if user.hasChild("tripCoordinate") {
+                            self.zoom(toFitAnnotationFromMapView: self.mapView)
+                            self.centerMapBtn.fadeTo(alhpaValue: 0.0, withDuration: 0.2)
+                        } else {
+                            self.centerMapOnUserLocation()
+                            self.centerMapBtn.fadeTo(alhpaValue: 0.0, withDuration: 0.2)
+                        }
+                    }
+                }
+            }
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
